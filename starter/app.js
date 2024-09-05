0d3b1f2e807df74e9ba621f955e6227e46d420d6
Jeyson Johnson <jeysonjohnson08@gmail.com>
2024-09-05 21:18:28 +1000

    set up .env
    set up environment variables and made sure the connection is made to the server only if the database connection is extrablished

--- starter/app.js
+++ starter/app.js
@@ -1,14 +1,27 @@
- require('./db/connect')
+ const connectDB = require('./db/connect')
+require("dotenv").config()
+
 const express = require("express")
+
+
 const app = express()
+
+const port = 3000;
 
 const tasks = require("./routes/tasks")
 app.use(express.json())
 app.use("/api/v1/tasks", tasks)
 
-app.get("/",(req,res)=>{
-    res.send("okiedokie")
-})
+
+const start = async()=>{
+    try{
+await connectDB(process.env.MONGO_URI);
+app.listen(port,()=>{console.log('server is listerning to port 3000')})}
+catch(error){
+    console.log("error");
+}
+}
+
+start()
 
 
-app.listen(3000,()=>{console.log('server is listerning to port 3000')})
