26e7acef8581d12461b2094b3478cf706d4d6507
Jeyson Johnson <jeysonjohnson08@gmail.com>
2024-09-12 01:56:57 +1000

    added a custom error handler"

--- starter/app.js
+++ starter/app.js
@@ -1,20 +1,21 @@
  const connectDB = require('./db/connect')
 require("dotenv").config()
 const notfound = require("./middleware/notfound")
-
+const errorHandlerMiddleware = require("./middleware/errorhandler")
 const express = require("express")
 
 
 const app = express()
 
 const port = 3000;
-app.use(notfound);
+
 const tasks = require("./routes/tasks")
 app.use(express.static("./public"))
 app.use(express.json())
 app.use("/api/v1/tasks", tasks)
 
-
+app.use(notfound);
+app.use(errorHandlerMiddleware)
 
 const start = async()=>{
     try{
